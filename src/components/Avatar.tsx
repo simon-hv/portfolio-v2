5c558e8151d74a87010b4025fc8bdd80a354565c
Simon Haïoun-Viet <simon12100@gmail.com>
2024-05-01 15:41:16 +0200

    feat: navbar

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,9 @@
+import { Avatar as AvatarCn, AvatarImage } from '@/components/ui/avatar';
+
+export const Avatar = () => {
+  return (
+    <AvatarCn>
+      <AvatarImage src="avatar.jpeg" alt="Simon Haïoun-Viet" />
+    </AvatarCn>
+  );
+};
