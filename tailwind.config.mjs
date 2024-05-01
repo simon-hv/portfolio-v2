e21925b62db3a961174aa4d5cd350c51d950ee20
Simon Haïoun-Viet <simon12100@gmail.com>
2024-05-01 20:13:43 +0200

    fix: improve loading animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -69,7 +69,6 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        fade: 'fade 0.6s ease-in',
       },
     },
   },
