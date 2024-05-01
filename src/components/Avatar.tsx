dcfef8a0a4357516eb8d5b8e5e80798b4fa48d9b
Simon Haïoun-Viet <simon12100@gmail.com>
2024-05-01 16:10:20 +0200

    fix: style polish

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,7 +3,7 @@ import { Avatar as AvatarCn, AvatarImage } from '@/components/ui/avatar';
 export const Avatar = () => {
   return (
     <AvatarCn>
-      <AvatarImage src="avatar.jpeg" alt="Simon Haïoun-Viet" />
+      <AvatarImage src="/avatar.jpeg" alt="Simon Haïoun-Viet" />
     </AvatarCn>
   );
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -61,12 +61,17 @@ export default {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        fade: {
+          from: { opacity: '0' },
+          to: { opacity: '1' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        fade: 'fade 0.6s ease-in',
       },
     },
   },
   plugins: [require('tailwindcss-animate')],
-};
+}
