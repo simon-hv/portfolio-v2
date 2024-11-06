c7541bdb8c9e8077d26375f9482bca157859cec3
Simon Haïoun-Viet <simon12100@gmail.com>
2024-11-06 16:34:30 +0100

    fix: improve Avatar component

--- src/components/Avatar.tsx
+++
@@ -1,9 +0,0 @@
-import { Avatar as AvatarCn, AvatarImage } from '@/components/ui/avatar';
-
-export const Avatar = () => {
-  return (
-    <AvatarCn>
-      <AvatarImage src="/avatar.jpeg" alt="Simon Haïoun-Viet" />
-    </AvatarCn>
-  );
-};
